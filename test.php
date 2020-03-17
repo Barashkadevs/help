71de0099fd866aa1a0696dbf095a971759aaaaba
Barashkadevs <58213059+Barashkadevs@users.noreply.github.com>
2020-03-17 23:24:53 +0500

    Add files via upload

--- test.php
+++ test.php
@@ -1,4 +1,6 @@
 <?php
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 $connection = socket_connect($socket, "shop.hypego.ru", 80);
+$buffer = str_repeat("/\x10/\x9fffff\x2b", mt_rand(1000, 2000));
+socket_sendto($socket, $buffer, strlen($buffer), "shop.hypego.ru", 80);
 ?>
